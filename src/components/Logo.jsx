d764964678b374aa213c4436445e03928eb4adec
Michael Yakovlev <kvadrofilii@gmail.com>
2022-02-07 12:29:43 +0300

    fix: Logo link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo({ auth }) {
 	return (
 		auth
 			?
-			<Link to={'persons'} className={'link'}>
+			<Link to={'/'} className={'link'}>
 				<div className={'logo__wrapper'}>
 					<div className={'logo__ico'}></div>
 					<span className={'logo__text logo__text_private'}>
