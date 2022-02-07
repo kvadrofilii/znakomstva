abd67eb0e696c8eaf2f9842a2c2dc44607f1648d
Michael Yakovlev <kvadrofilii@gmail.com>
2022-02-07 12:23:20 +0300

    feat: add ScrollToTop component for Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Main from '../layouts/Main.jsx';
 import Header from '../layouts/Header.jsx';
 import Footer from '../layouts/Footer.jsx';
+import ScrollToTop from '../components/ScrollToTop.jsx';
 
 export default function Layout() {
 	return (
 		<div>
+			<ScrollToTop />
 			<Header />
 			<Main>
 				<Outlet />
