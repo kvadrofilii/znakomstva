e75804f4a8746e2caae18d049e5108c28f616bf3
Michael Yakovlev <kvadrofilii@gmail.com>
2022-03-02 11:29:43 +0300

    fix: img links for project

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
-import { HashRouter } from 'react-router-dom';
+//import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*<BrowserRouter>*/}
-    <HashRouter>
+    <BrowserRouter>
+      {/*<HashRouter>*/}
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
-    {/*</BrowserRouter>*/}
+      {/*</HashRouter>*/}
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
